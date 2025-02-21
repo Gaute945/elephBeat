6bd19db8db72830a3f0373e8b6858bec4fe80836
Gaute <mrboobai@proton.me>
2025-02-21 16:16:54 +0100

    start on object convertion

--- src/collector.ts
+++ src/collector.ts
@@ -1,17 +1,25 @@
 const cpuQuery = '100 * pve_cpu_usage_ratio{job="proxmox_server"}';
-const cpuUrl: string = `http://192.168.10.44:9090/api/v1/query?query=${encodeURIComponent(cpuQuery)}`;
-
-// RAM usage per CT/VM in %
-const ramPercentQuery = '100 * (pve_memory_usage_bytes{job="proxmox_server"} / pve_memory_size_bytes{job="proxmox_server"})';
-const ramUrl: string = `http://192.168.10.44:9090/api/v1/query?query=${encodeURIComponent(ramPercentQuery)}`;
-
-// Bootdisk usage per CT/VM in %
-const bootdiskPercentQuery = '100 * (pve_disk_usage_bytes{job="proxmox_server"} / pve_disk_size_bytes{job="proxmox_server"})';
-const bootdiskUrl: string = `http://192.168.10.44:9090/api/v1/query?query=${encodeURIComponent(bootdiskPercentQuery)}`;
-
-// Status check for running/online VMs/CTs
+const ramQuery = '100 * (pve_memory_usage_bytes{job="proxmox_server"} / pve_memory_size_bytes{job="proxmox_server"})';
+const bootdiskQuery = '100 * (pve_disk_usage_bytes{job="proxmox_server"} / pve_disk_size_bytes{job="proxmox_server"})';
 const statusQuery = 'pve_up or pve_storage_up or pve_lxc_up or pve_qemu_up';
-const statusUrl = `http://192.168.10.44:9090/api/v1/query?query=${encodeURIComponent(statusQuery)}`;
+
+const ip: string = '192.168.10.44';
+const port: string = '9090';
+let queries = []
+
+async function dataPrep(query: string) {
+  return fetch(`http://${ip}:${port}/api/v1/query?query=${encodeURIComponent(query)}`, { method: "GET", headers: { "Content-Type": "application/json" } })
+    .then(Response => {
+      return Response.json()
+    })
+};
+
+queries = [cpuQuery, ramQuery, bootdiskQuery, statusQuery];
+queries.forEach(query => {
+  dataPrep(query).then(result => {
+    console.log(JSON.stringify(result, null, 2));
+  })
+});
 
 export const metrics = new Map();
 
@@ -37,40 +45,12 @@ function add2map(data: any, identifier: string) {
       }
     }
   }
-  else {
-    console.log("Either failed status or empty data for", data);
-    if (!Array.isArray(data.data.result)) {
-      console.log("data.data.result is not an array:", data.data.result);
-    } else if (data.data.result.length === 0) {
-      console.log("data.data.result is empty:", data.data.result);
-    }
-  }
 }
 
 const fetchOptions = { method: "GET", headers: { "Content-Type": "application/json" } };
 async function getMetrics() {
   metrics.clear()
 
-  const [cpuResponse, ramResponse, bootdiskResponse, statusResponse] = await Promise.all([
-    fetch(cpuUrl, fetchOptions),
-    fetch(ramUrl, fetchOptions),
-    fetch(bootdiskUrl, fetchOptions),
-    fetch(statusUrl, fetchOptions)
-  ]);
-
-  const [cpuData, ramData, bootdiskData, statusData] = await Promise.all([
-    cpuResponse.json(),
-    ramResponse.json(),
-    bootdiskResponse.json(),
-    statusResponse.json()
-  ]);
-
-  add2map(cpuData, "cpu");
-  add2map(ramData, "ram");
-  add2map(bootdiskData, "bootdisk");
-  add2map(statusData, "status");
-
-  console.log(metrics);
   return metrics
 };
 
