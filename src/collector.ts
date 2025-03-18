0ff4cb39c0175b97ea5c7c30de712050765e8013
Gaute <mrboobai@proton.me>
2025-03-18 16:04:35 +0100

    data structure working and cpu done

--- src/collector.ts
+++ src/collector.ts
@@ -6,26 +6,59 @@ const statusQuery = 'pve_up or pve_storage_up or pve_lxc_up or pve_qemu_up';
 const ip: string = '192.168.10.44';
 const port: string = '9090';
 
-let machines: any = [];
+interface machinesType {
+  name: string;
+  cpu: number;
+  bootdisk: number;
+  status: number;
+}
+
+let machines: machinesType[] = [];
 let queries = [cpuQuery, ramQuery, bootdiskQuery, statusQuery];
 
-async function dataPrep(query: string) {
-  return fetch(`http://${ip}:${port}/api/v1/query?query=${encodeURIComponent(query)}`, { method: "GET", headers: { "Content-Type": "application/json" } })
-    .then(Response => {
-      return Response.json()
-    })
+async function query(query: string) {
+  const response = await fetch(`http://${ip}:${port}/api/v1/query?query=${encodeURIComponent(query)}`, { method: "GET", headers: { "Content-Type": "application/json" } })
+  return response.json();
 };
 
-dataPrep(statusQuery).then(result => {
+async function populateMachines() {
+  const result = await query(statusQuery);
   for (let i = 0; i < result.data.result.length; i++) {
-    console.log(result.data.result[i].metric.id)
-    machines.unshift(result.data.result[i].metric.id)
+    const name: string = result.data.result[i].metric.id;
+    const machine = {
+      name,
+      cpu: 0,
+      ram: 0,
+      bootdisk: 0,
+      status: 0
+    };
+    machines.push(machine);
   }
-});
+  // console.log(machines);
+};
 
-//dataPrep(statusQuery).then(result => {
+async function update() {
+  const result = await query(cpuQuery);
+  //console.log(JSON.stringify(result));
+  for (let i = 0; i < result.data.result.length; i++) {
+    const fetchId = result.data.result[i].metric.id;
+    machines.forEach(machine => {
+      if (machine.name === fetchId) {
+        machine.cpu = +result.data.result[i].value[1];
+      }
+    });
+  }
+}
+
+async function main() {
+  await populateMachines();
+  await update();
+  console.log(machines);
+}
+
+main();
+
 //  console.log(JSON.stringify(result, null, 2))
-//});
 
 /*
 per 1min
